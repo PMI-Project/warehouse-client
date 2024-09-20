ef60313dc31ab8f7d0f849a6488c41c533c11781
Aji Prio <ajiprio@hotmail.com>
2024-09-20 20:11:27 +0700

    PP-7 [FE] Login Page add: Client login hit api endpoint

--- auth.config.ts
+++ auth.config.ts
@@ -18,27 +18,46 @@ const authConfig = {
         }
       },
       async authorize(credentials, req) {
-        console.log(req);
-        const user = {
-          id: '1',
-          name: 'John',
-          email: credentials?.email as string
-        };
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
+        const response = await fetch(
+          `${process.env.AUTH_URL_DEV}/api/v1/auth/login`,
+          {
+            method: 'POST',
+            body: JSON.stringify(credentials),
+            headers: { 'Content-Type': 'application/json' }
+          }
+        );
 
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        const data = await response.json();
+
+        if (response.ok && data) {
+          return {
+            ...data.data.user,
+            accessToken: data.data.accessToken,
+            refreshToken: data.data.refreshToken
+          };
+        } else {
+          return null;
         }
       }
     })
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user = token.user as any;
+
+      return session;
+    }
+  },
   pages: {
     signIn: '/' //sigin page
-  }
+  },
+  trustHost: true
 } satisfies NextAuthConfig;
 
 export default authConfig;
