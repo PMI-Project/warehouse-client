1dec3311e126b823a51d29db19cf09e9974ce860
Aji Prio <ajiprio@hotmail.com>
2024-12-02 14:13:14 +0700

    update pipeline

--- lib/signalr.ts
+++ lib/signalr.ts
@@ -1,6 +1,5 @@
 import * as signalR from '@microsoft/signalr';
 
 export const conn = new signalR.HubConnectionBuilder()
-  .withUrl('http://localhost:8080/deviceHub')
-  .configureLogging(signalR.LogLevel.Information)
+  .withUrl(`${process.env.NEXT_PUBLIC_DEVICE_HUB}`)
   .build();
